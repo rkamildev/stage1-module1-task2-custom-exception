bdfbd7da1e5f0004c9148b5ab13c34a20e59de26
rkamildev <rkamil.int@gmail.com>
2023-07-27 17:39:17 +0500

    Exceptions

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,24 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException {
+    boolean inRange = false;
+    for (int i = 0; i < IDs.length; i++) {
+      if (IDs[i] == studentID)
+        inRange = true;
+    }
+    if (!inRange || Student.getValueOf(studentID) == null)
+      throw new StudentNotFoundException("Could not find student with ID " + studentID);
+    else
+      return Student.getValueOf(studentID);
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
     }
-
   }
 }
